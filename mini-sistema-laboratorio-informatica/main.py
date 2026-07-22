757fa8e6fa859f6b0e30c9a278ee053daffd6823
ridelfis <ridelfisenmanuelfranco@gmail.com>
2026-07-22 19:36:05 -0400

    Creacion de la funcion para mostrar el menu principal :: gestion-laboratorio-informatica

--- mini-sistema-laboratorio-informatica/main.py
+++ mini-sistema-laboratorio-informatica/main.py
@@ -3,4 +3,30 @@ print('***[ SISTEMA DE GESTION DE LABORATORIO DE INFORMATICA ]***')
 #                               SISTEMA DE GESTION DE LABORATORIO DE INFORMATICA
 # ==============================================================================================
 
+# ==============================================================================================
+#                                           DATOS 
+# ==============================================================================================
+computadoras = []
+contador = len(computadoras) + 1
+
+
+# ==============================================================================================
+#                               MENU PRINCIPAL DEL SISTEMA
+# ==============================================================================================
+def mostrar_menu_principal():
+    print('''
+    ==========================================
+       SISTEMA DE LABORATORIO DE INFORMÁTICA
+    ==========================================
+    [1] Registrar computadora.
+    [2] Mostrar computadoras.
+    [3] Buscar computadora.
+    [4] Enviar a mantenimiento.
+    [5] Finalizar mantenimiento.
+    [6] Mostrar computadoras disponibles.
+    [7] Mostrar computadoras en mantenimiento.
+    [8] Eliminar computadora.
+    [9] Salir.
+    ==========================================
+    ''')
 
